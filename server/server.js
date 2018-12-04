aa397eb2b286dc133a2e386116d5381e3abf4cdd
Cheri Portman <cheriportman@hotmail.com>
2018-12-04 15:42:01 -0800

    server is talking

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
-const express = requrire ('express');
+const express = require('express');
 const app = express();
-const shortid = require('shortid');
-
 const fs = require('fs');
 
 function readData() {
-    const data = fs.readFileSync('')
+  const data = fs.readFileSync('./data/books.json', 'utf8');
+  return JSON.parse(data);
 }
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  const books = readData();
+  console.log(books);
+});
+
+// this should go at the bottom //
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log('server app started on port', PORT);
+});
