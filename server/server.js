1b3854e0bf8428807472d01c7cc8b60105863216
Cari Palazzolo <cari.pizza@gmail.com>
2018-12-04 15:23:46 -0800

    server.js and package.json installed express

--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
-console.log('this is a test');
+console.log('this is a cool test');
 
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 // const shortid = require('shortid');
 
-// const fs = require('fs');
+const fs = require('fs');
+
+function readData() {
+  const data = fs.readFileSync('./data/synths.json', 'utf8');
+  return JSON.parse(data);
+}
+
+app.use(express.json());
+
+app.get('https://localhost:3000/api/synths', (req, res) => {
+  const synths = readData();
+  res.json(synths);
+});
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log('server app started on port', PORT);
+});
