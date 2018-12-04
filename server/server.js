e1c49c18f6c3320249926ac5a7f30bece837844a
Aaron Dennis <AMDennis1987@gmail.com>
2018-12-04 14:01:02 -0800

    GET working for all guitarists

--- server/server.js
+++ server/server.js
@@ -1,1 +1,34 @@
 const imported = require('path');
+const express = require('express');
+const app = express();
+const shortid = require('shortid');
+
+const fs = require('fs');
+
+function readData() {
+  const data = fs.readFileSync('./data/guitarists.json', 'utf8');
+  return JSON.parse(data);
+}
+
+app.use(express.json());
+
+app.get('/api/guitarists', (req, res) => {
+  const guitarists = readData();
+
+  if(req.query.name) {
+    const match = req.query.name.toLowerCase();
+    const filtered = guitarists.filter(s => {
+      return s.name.toLowerCase().startsWith(match);
+    });
+    res.json(filtered);
+  }
+  else {
+    res.json(guitarists);
+  }
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log('server app started on port', PORT);
+});
