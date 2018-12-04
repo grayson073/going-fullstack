e018f1b694bbd9adbd3cca4143bd9c5ca303c57f
Cheri Portman <cheriportman@hotmail.com>
2018-12-04 14:30:04 -0800

    package.json changes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log('what');
+const express = requrire ('express');
+const app = express();
+const shortid = require('shortid');
+
+const fs = require('fs');
+
+function readData() {
+    const data = fs.readFileSync('')
+}
