0ef3adfea23b9cf87b00bc4f8d0ae9ae8cb7c156
Dyanna Tolman <dyannast@gmail.com>
2018-12-04 15:01:33 -0800

    getting database, and showing up on terminal

--- server/server.js
+++ server/server.js
@@ -1,1 +1,25 @@
-console.log('running');
+const express = require('express');
+const app = express();
+
+const fs = require('fs');
+
+function readData() {
+  const data = fs.readFileSync('./data/movies.json', 'utf8');
+  return JSON.parse(data);
+}
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  const movies = readData();
+  console.log('movies', movies);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log('server app started on port', PORT);
+
+});
+
+
