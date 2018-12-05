9fd13cc3826278b5f608842f2acc2aefd1fddd6d
Kristin Hortsch <kristinhortsch@gmail.com>
2018-12-05 13:43:33 -0800

    i can see both console logs when i run drop-tables.js and create-tables.js in terminal

--- server/scripts/drop-tables.js
+++ server/scripts/drop-tables.js
@@ -2,14 +2,14 @@ const pg = require('pg');
 
 const Client = pg.Client;
 
-const databaseUrl = 'postgres://localhost:5432/school';
+const databaseUrl = 'postgres://localhost:5432/animal';
 
 const client = new Client(databaseUrl);
 
 client.connect()
   .then(() => {
     return client.query(`
-      DROP TABLE IF EXISTS students;
+      DROP TABLE IF EXISTS animals;
     `);
   })
   .then(
