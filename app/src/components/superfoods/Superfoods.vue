b08add8ab9a900178516faf8bcacabd860579640
Carmen V Ramos <carmenvramos@yahoo.com>
2018-12-04 17:30:17 -0800

    Superfoods list showing on page

--- app/src/components/superfoods/Superfoods.vue
+++ app/src/components/superfoods/Superfoods.vue
@@ -1,3 +1,37 @@
+<template>
+  <section class="superfoods">
+    <h2>Superfoods</h2>
+    <ul v-if="superfoods">
+      <li v-for="superfood in superfoods" :key="superfood.id">
+        {{superfood.name}}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import api from '../../services/api';
+export default {
+  data() {
+    return {
+      superfoods: null
+    };
+  },
+ 
+  created() {
+    api.getSuperfoods()
+      .then(superfoods => {
+        this.superfoods = superfoods;
+      });
+  }
+  
+  
+};
+</script>
+
+<style scoped>
+</style>
+
 
 
 
