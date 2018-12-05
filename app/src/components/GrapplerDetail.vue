b83a0f742883eeeee04713523da7a3037b1c4b27
Lance Owen Merrill <Lanceom@hawaii.edu>
2018-12-05 15:34:15 -0800

    grappler detail view has been created and the router links but the detail info isnt rendering

---
+++ app/src/components/GrapplerDetail.vue
@@ -0,0 +1,30 @@
+<template>
+    <div v-if="grappler">
+        <h2> Name: {{grappler.name}}</h2>
+        <h3> Age:{{grappler.age}}</h3>
+        <h3>Championship Status: {{grappler.champ}}</h3>
+
+    </div>
+</template>
+
+<script>
+import api from '../services/api';
+export default {
+    data() {
+        return {
+            grappler: null
+        };
+    }, 
+    created() {
+        api.getGrappler(this.$route.params.id)
+            .then(grappler => {
+                this.grappler = grappler;
+            });
+    }
+
+};
+</script>
+
+<style>
+
+</style>
--- app/src/router.js
+++ app/src/router.js
@@ -1,11 +1,13 @@
 import VueRouter from 'vue-router';
 import Home from './components/Home.vue';
 import Grapplers from './components/Grapplers.vue';
+import GrapplerDetail from './components/GrapplerDetail';
 
 
 export default new VueRouter ({
     routes: [
         { path: '/', component: Home }, 
-        { path: '/grapplers', component: Grapplers }
+        { path: '/grapplers', component: Grapplers }, 
+        { path: '/grapplerdetail', component: GrapplerDetail }
     ]
 });
