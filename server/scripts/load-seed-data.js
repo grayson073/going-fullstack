f5b80f0013931225c6163511e738f5ef6bf3ee1d
Aaron Dennis <AMDennis1987@gmail.com>
2018-12-05 14:00:46 -0800

    seed data working

--- server/scripts/load-seed-data.js
+++ server/scripts/load-seed-data.js
@@ -1,20 +1,19 @@
 const pg = require('pg');
 const Client = pg.Client;
 const databaseUrl = 'postgres://localhost:5432/guitarists';
-const guitarists = require('./guitarists.json');
+const guitarists = require('../data/guitarists.json');
 
 const client = new Client(databaseUrl);
 
 client.connect()
   .then(() => {
-    // "Promise all" does a parallel execution of async tasks
     return Promise.all(
       guitarists.map(guitarist => {
         return client.query(`
           INSERT INTO guitarists (name, music_type, yob)
           VALUES ($1, $2, $3);
         `,
-        [guitarist.name, guitarist.music_type, guitarist.yob]);
+        [guitarist.name, guitarist.musicType, guitarist.yob]);
       })
     );
   })
