3ff6df0aecf340c9a67d1b44480e3f9576419f21
Carmen V Ramos <carmenvramos@yahoo.com>
2018-12-04 17:54:24 -0800

    Added form to add new resource, list is updated to reflect the newly added resource

---
+++ app/src/components/superfoods/AddSuperfood.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input v-model="superfood.name" require>
+    <button>Add</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    onAdd: Function
+  },
+  data() {
+    return {
+      superfood: {
+        name: ''
+      }
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.onAdd(this.superfood)
+        .then(() => {
+          this.superfood = { name: '' };
+        });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- app/src/components/superfoods/Superfoods.vue
+++ app/src/components/superfoods/Superfoods.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="superfoods">
     <h2>Superfoods</h2>
+
+    <AddSuperfood :onAdd="handleAdd"/>
     <ul v-if="superfoods">
       <li v-for="superfood in superfoods" :key="superfood.id">
         {{superfood.name}}
@@ -11,18 +13,30 @@
 
 <script>
 import api from '../../services/api';
+import AddSuperfood from './AddSuperfood';
 export default {
   data() {
     return {
       superfoods: null
     };
   },
- 
+  components: {
+    AddSuperfood
+  },
   created() {
     api.getSuperfoods()
       .then(superfoods => {
         this.superfoods = superfoods;
       });
+  },
+  methods: {
+    handleAdd(superfood) {
+      return api.addSuperfood(superfood)
+        .then(saved => {
+          console.log('saved', saved);
+          // this.students.push(saved);
+        });
+    }
   }
   
   
