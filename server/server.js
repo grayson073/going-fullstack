3a471794bc1558482b89d0a3dcda40a10334da2c
Cheri Portman <cheriportman@hotmail.com>
2018-12-04 20:50:40 -0800

    one error left

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const shortid = require('shortid');
 
 const fs = require('fs');
 
@@ -8,13 +9,40 @@ function readData() {
   return JSON.parse(data);
 }
 
+function saveData(books) {
+  const json = JSON.stringify(books, true, 2);
+  fs.writeFileSync('./data/books.json', json);
+}
+
 app.use(express.json());
 
 app.get('/api/books', (req, res) => {
   const books = readData();
-  res.json(books);
+  // res.json(books);
+  
+  if(req.query.title) {
+    const match = req.query.title.toLowerCase();
+    const filtered = books.filter(b => {
+      return b.title.toLowerCase().startsWith(match);
+    });
+    res.json(filtered);
+  }
+  else {
+    res.json(books);
+  }
 });
 
+app.post('/api/books', (req, res) => {
+
+  const books = readData();
+  const book = req.body;
+  book.id = shortid.generate();
+  
+  books.push(book);
+  saveData(books);
+
+  res.json(book);
+});
 // this should go at the bottom //
 const PORT = 3000;
 
