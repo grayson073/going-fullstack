f71b5393d7facd93969182f9ec0fe38f55f3693d
Connor Frendt <cjacksonfx@yahoo.com>
2018-12-06 15:20:12 -0800

    APP updated add form to include true or false boolean value

--- app/src/components/movies/AddMovie.vue
+++ app/src/components/movies/AddMovie.vue
@@ -10,16 +10,23 @@
         v-model="movie.year" required>
     </p>
 
-    <!-- <p>
-      Rating?
-    </p> -->
-
+    <p id="rating">
+      <label>Rating:</label>
+      <br />
+      <label for="good">Good</label>
+      <input type="radio" id="good" name="radio-button" value="true"
+        v-model="movie.rating" required>
+        <br />
+      <label for="bad">Bad</label>
+      <input type="radio" id="bad" name="radio-button" value="false"
+        v-model="movie.rating" required>
+    </p>
+    <br />
     <button>Add</button>
   </form>
 </template>
 
 <script>
-import api from '../../services/api';
 
 function initMovie() {
   return {
@@ -49,5 +56,9 @@ export default {
 </script>
 
 <style>
-
+#rating {
+  display: inline-block;
+  border: 1px solid black;
+  padding: 10px;
+}
 </style>
