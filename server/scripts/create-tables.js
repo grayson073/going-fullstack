61864837e4fa6631748e5b9c645f6612f0f89f54
Kristin Hortsch <kristinhortsch@gmail.com>
2018-12-06 14:33:16 -0800

    updated create-tables for new size table, can see in psql

--- server/scripts/create-tables.js
+++ server/scripts/create-tables.js
@@ -1,15 +1,23 @@
 const client = require('../db-client');
 
 client.query(`
-      CREATE TABLE IF NOT EXISTS animals (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(256) NOT NULL,
-        weight INT,
-        mammal BOOLEAN,
-        SIZE VARCHAR(256),
-        image VARCHAR(256)
-      );
-    `)
+  CREATE TABLE IF NOT EXISTS size (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    short_name VARCHAR(1) NOT NULL
+  );
+
+
+  CREATE TABLE IF NOT EXISTS animals (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    weight INT,
+    mammal BOOLEAN,
+    size VARCHAR(256),
+    image VARCHAR(256)
+  );
+
+`)
   
   .then(
     () => console.log('create tables complete'),
