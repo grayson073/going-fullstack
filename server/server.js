deffc287a5234b5d6ac17487b10e0b5461735ad4
Ivan <i.poblete915@gmail.com>
2018-12-05 22:53:22 -0800

    tried to link api but failed

--- server/server.js
+++ server/server.js
@@ -20,6 +20,29 @@ app.get('/api/superheroes', (req, res) => {
     });
 });
 
+app.get('/api/superheroes/:name', (req, res) => {
+  client.query(`
+    SELECT * FROM superheroes WHERE name = $1;
+  `,
+  [req.params.id])
+    .then(result => {
+      res.json(result.rows[0]);
+    });
+});
+
+app.post('/api/superheroes', (req, res) => {
+  const body = req.body;
+
+  client.query(`
+    INSERT INTO superheroes (name, age, id)
+    VALUES($1, $2, $3)
+    RETURNING name, age, id;
+  `,
+  [body.name, body.age])
+    .then(result => {
+      res.json(result.rows[0]);
+    });
+});
 
 const PORT = 3000;
 
