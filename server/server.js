b161b343e31977a3ec3d2d7822d371fe71e9914e
Shaba9 <shaba92@gmail.com>
2018-12-05 16:05:54 -0800

    api gets data from database table and displays on page

--- server/server.js
+++ server/server.js
@@ -3,6 +3,22 @@ const app = express();
 // const shortid = require('shortid');
 const pg = require('pg');
 
+app.use(express.json());
+
+const Client = pg.Client;
+const dbUrl = 'postgres://shaba:123@localhost:5432/emo';
+const client = new Client(dbUrl);
+client.connect();
+
+app.get('/api/emojis', (req, res) => {
+  client.query(`
+    SELECT * FROM emojis;
+  `)
+    .then(result => {
+      res.json(result.rows);
+    });
+});
+
 
 const PORT = 3000;
 
