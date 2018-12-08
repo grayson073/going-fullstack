e1081a9a6afd1f45d1546efb2282264f2f84a69e
Kevin Grayson <k.grayson.ms@gmail.com>
2018-12-07 22:06:54 -0800

    Updated games GET route to include genre table join

--- server/lib/routes/games.js
+++ server/lib/routes/games.js
@@ -7,12 +7,15 @@ router
   .get('/', (req, res) => {
     client.query(`
       SELECT
-        id,
-        title,
-        year,
-        image_url as "imageUrl",
-        genre_id as "genreId"
+        game.id,
+        game.title,
+        game.year,
+        game.image_url as "imageUrl",
+        genre.id as "genreId",
+        genre.category as genre
       FROM game
+      JOIN genre
+      ON game.genre_id = genre.id
       ORDER BY title;
     `)
       .then(result => {
