6b96b7fc6548f152d49567f15f566f3e6aa99154
Kevin Grayson <k.grayson.ms@gmail.com>
2018-12-07 21:24:50 -0800

    Added mock data for games, genres

---
+++ server/scripts/genres-data.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { genre: 'Action', shortName: 'act' },
+  { genre: 'Shooter', shortName: 'sh' },
+  { genre: 'Adventure', shortName: 'adv' },
+  { genre: 'Sports', shortName: 'sp' },
+];
